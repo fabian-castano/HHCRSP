c7c8424714cdd7b9b0dd3bf5a29aba26c442d3bc
fabiancastano <fabian.castano@frubana.com>
2023-12-11 08:14:20 -0500

    changes to Read instances from a single file

--- proyecto_HHCRSP/main.py
+++ proyecto_HHCRSP/main.py
@@ -16,12 +16,12 @@ from app.libs.constants import shift_type,HMM,DM
 if __name__=="__main__":
 
     
-    """df=construct_model('instances/',sys.argv[1])
+    df=construct_model('instances/',sys.argv[1])
     if not os.path.exists('results/'):
         os.makedirs('results/')
-    df.to_pickle(f"results/results_{sys.argv[1]}.pkl")"""
+    df.to_pickle(f"results/results_{sys.argv[1]}.pkl")
 
-    working_dir='results/'
+    """working_dir='results/'
     dfs=[]
     for i in range(3,172):
         df=pd.read_pickle(f"{working_dir}results_{i}.pkl")
@@ -29,3 +29,4 @@ if __name__=="__main__":
         dfs.append(df)
     df=pd.concat(dfs)
     df.to_csv(f"{working_dir}general_results.csv")
+    """
